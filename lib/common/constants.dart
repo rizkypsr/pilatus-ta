e8bd88dd703e0bd2ab6bb2db6a40120214e149bf
abyadh <69466943+abyadh@users.noreply.github.com>
2022-07-12 13:37:26 +0700

    fix

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,7 +1,5 @@
-const baseUrlApi =
-    'https://ed7d-2001-448a-6060-44cc-5d23-84d6-1191-7e24.ap.ngrok.io/api/v1';
-const baseUrl =
-    'https://ed7d-2001-448a-6060-44cc-5d23-84d6-1191-7e24.ap.ngrok.io';
+const baseUrlApi = 'https://90ac-125-164-96-91.ap.ngrok.io/api/v1';
+const baseUrl = 'https://90ac-125-164-96-91.ap.ngrok.io';
 const rajaOngkirUrl = "https://api.rajaongkir.com/starter";
 const rajaOngkirApiKey = "3d00bc0e84b65a813d00a1c6469b11ad";
-const token = "1|z0Gbeie2ims7IogcQT7p0fN9CaxIyMujWz7BJ8mD";
+const token = "2|fB2X7jLFneGpxvILrT4asL0A6L5ycTAUS4jjIAwo";
